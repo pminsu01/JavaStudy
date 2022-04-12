6b464eb3ca12e7b209cd4df3adcc1a817953a605
Erjuer <erjuer01@gmail.com>
2022-04-13 01:01:06 +0900

    [Add] 함수형 인터페이스 Function 예시 코드 추가

--- src/functionInterface/FunctionEx.java
+++ src/functionInterface/FunctionEx.java
@@ -1,13 +1,15 @@
 package functionInterface;
 
 import generic.Meat;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
 public class FunctionEx {
 
 
-  public <T,String> Function<T,String> FunctionEx() {
+  // T,R : 제네릭 T를 받아서 R을 리턴한다.
+  public <T,String> Function<T,String> paymentInterFaceByMeat() {
 
     return new Function<T, String>() {
       @Override
@@ -18,9 +20,21 @@ public class FunctionEx {
         if(Objects.equals(meat.getKindValue(), "한우")) return (String) "임원 법인카드";
         if(Objects.equals(meat.getKindValue(), "양")) return (String) "팀장 법인카드";
         if(Objects.equals(meat.getKindValue(), "돼지")) return (String) "부가급 나온달 개인카드";
-        if(Objects.equals(meat.getKindValue(), "닭")) return (String) "개인카드";
-        return (String) "굶자";
+        if(Objects.equals(meat.getKindValue(), "닭")) return (String) "부모님 카드";
+        return (String) "개인카드";
       }
     };
   }
+
+  public <T> void FunctionMethod(List<Meat> meatList, Function<T, Object> functionIf) {
+
+        for (Meat meat: meatList) {
+          String cardValue = (String) functionIf.apply((T) meat);
+          System.out.println(meat.getKindValue() + "을/를 먹을 때에는 " + cardValue +"로 결제해야 한다.");
+  }
+
+}
+
+
+
 }
--- src/functionInterface/functionInterface.java
+++ src/functionInterface/functionInterface.java
@@ -1,23 +1,29 @@
 package functionInterface;
 
+import generic.Meat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class functionInterface {
 
     public static void main(String[] arg){
 
-//        PredicateEx predicateEx = new PredicateEx();
-//        predicateEx.PredicateRunMethod();
+/*      PredicateEx predicateEx = new PredicateEx();
+        predicateEx.PredicateRunMethod();*/
 
 
-        ConsumerEx consumerEx = new ConsumerEx();
-        consumerEx.consumerMethod();
+/*      ConsumerEx consumerEx = new ConsumerEx();
+        consumerEx.consumerMethod();*/
 
+        List<Meat> meatList = Arrays.asList(new Meat(400,"한우"), new Meat(500,"돼지"), new Meat(300,"닭"), new Meat(150,"닭가슴살"));
+        FunctionEx functionEx = new FunctionEx();
+        functionEx.FunctionMethod(meatList, functionEx.paymentInterFaceByMeat());
 
+        meatList.stream().map(functionEx.paymentInterFaceByMeat()).forEach(System.out::println);
 
 
     }
